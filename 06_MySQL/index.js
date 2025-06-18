08001e12d1b5a5f059e602d8d69b503f8600fe37
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-06-18 14:50:30 +0530

    Complete Backend

--- 06_MySQL/index.js
+++ 06_MySQL/index.js
@@ -1,5 +1,23 @@
 import { faker } from "@faker-js/faker"
 import mysql from "mysql2"
+import express from "express"
+import path from "path"
+import methodOverride from "method-override"
+import { fileURLToPath } from "url"
+
+const app = express()
+const port = 3000
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(methodOverride("_method"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.set("view engine", "views")
+app.set(path.join(__dirname, "views"))
+app.use(express.static(path.join(__dirname, "public")))
 
 const randomUser = () => {
   return [
@@ -10,11 +28,6 @@ const randomUser = () => {
   ];
 }
 
-let data = []
-for (let i = 0; i <= 100; i++) {
-  data.push(randomUser())
-}
-
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -22,15 +35,33 @@ const connection = mysql.createConnection({
   password: "SQL@FIRSTDB01"
 })
 
-const q = "INSERT INTO user(id, username, email, password) VALUES ?;"  // write placeholder outside brackets
+app.get("/user", (req, res) => {
+  const q = "SELECT * FROM user;"
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err
+      const users = result
+      res.render("User.ejs", { users })
+    })
+  } catch (err) {
+    console.log(err)
+  }
+})
 
-try {
-  connection.query(q, [data], (error, results) => { // use the brackets for multiple values
-    if (error) throw error;
-    console.log(results)
-  })
-} catch (error) {
-  console.log(error)
-}
+app.get("/user/edit/:id", (req, res) => {
+  const { id } = req.params
+  const q = `SELECT * FROM user WHERE id = "${id}"` // make string of this id
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err
+      const data = result[0]
+      res.render("Edit.ejs", { data })
+    })
+  } catch (err) {
+    console.log(err)
+  }
+})
 
-connection.end()
+app.listen(port, () => {
+  console.log(`app is listening on the port no ${port}`)
+})
